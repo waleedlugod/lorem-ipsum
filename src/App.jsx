a09d00158acb4d1282b13a1e46b25b7ad5e06180
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-20 21:11:25 +0800

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data/data";
 
 function App() {
+	const [numPs, setNumPs] = useState("");
+	const [text, setText] = useState([]);
+
+	function handleChange(e) {
+		setNumPs(e.target.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		setText(data.slice(0, numPs));
+	}
+
 	return (
-		<>
-			<h2>TIRED OF BORING LOREM IPSUM?</h2>
-		</>
+		<div className="section-center">
+			<h3>TIRED OF BORING LOREM IPSUM?</h3>
+			<form className="lorem-form" onSubmit={handleSubmit}>
+				<label htmlFor="paragraphs">Paragraphs:</label>
+				<input
+					type="text"
+					id="paragraphs"
+					value={numPs}
+					onChange={handleChange}
+				/>
+			</form>
+			{text.map((paragraph) => {
+				return <p>{paragraph}</p>;
+			})}
+		</div>
 	);
 }
 
